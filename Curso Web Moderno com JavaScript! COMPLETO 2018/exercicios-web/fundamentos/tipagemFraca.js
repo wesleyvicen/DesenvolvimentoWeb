9be0629879914b25e7db6d592d91c6f868ec71ea
Wesley Vicente <wesley1535@hotmail.com>
2018-07-07 00:22:44 -0300

    Começo da aula de JavaScript

---
+++ Curso Web Moderno com JavaScript! COMPLETO 2018/exercicios-web/fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = 'legal'
+console.log(qualquer)
+console.log('é do tipo: ' + typeof qualquer)
+
+qualquer = 3.1234
+console.log(qualquer)
+console.log('é do tipo: ' + typeof qualquer)
+
+//Evitar nomes genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico perigoso... kkk
